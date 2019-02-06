bf88bcbef728ca4e8079bfebd55721d1989f78f8
Tech-A45 <tech-a45@Tech-A45xs-MacBook-Air.local>
2019-02-06 13:54:33 +0000

    created a test to check if the last name field is filled in

--- lib/pages/registration.rb
+++ lib/pages/registration.rb
@@ -7,6 +7,7 @@ class Registration
   # page objects
   SPARTA_REG_URL = 'http://localhost:9292/'
   FIRST_NAME_FIELD_ID = 'firstName'
+  LAST_NAME_FIELD_ID = 'lastName'
 
   def visit_registration_page
     visit(SPARTA_REG_URL)
@@ -14,6 +15,10 @@ class Registration
 
   def fill_in_firstname_field(name)
     fill_in(FIRST_NAME_FIELD_ID, :with => name)
+  end
+
+  def fill_in_lastname_field(name)
+    fill_in(LAST_NAME_FIELD_ID, :with => name)
   end
 
 end
--- spec/registration_spec.rb
+++ spec/registration_spec.rb
@@ -8,6 +8,7 @@ describe 'Testing a correct path for sparta registration' do
       @sparta_demosite = SpartaDemoSite.new.registration_page
       @sparta_demosite.visit_registration_page
       @sparta_demosite.fill_in_firstname_field('Kirpal')
+      @sparta_demosite.fill_in_lastname_field('Seehra')
 
       sleep 10
     end
